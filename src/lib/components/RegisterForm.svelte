81a486c68335142aff996fc9d020e246e6a9ddd7
Allaine Tan <allainericci@gmail.com>
2024-02-20 12:02:04 +0800

    fix: update wrong form label

--- src/lib/components/RegisterForm.svelte
+++ src/lib/components/RegisterForm.svelte
@@ -102,14 +102,14 @@
         for="website-admin" 
         class="block mb-2 text-sm font-medium text-gray-900"
     >
-        Username
+        Email
     </label>
     <div class="flex">
         <input 
             type="text" 
             id="website-admin" 
             class="rounded-s-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            placeholder="Bonnie Green"
+            placeholder="jdelacruz"
             required
         />
         <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-e-lg ">
