0b4c91a80bc81304af653d89f76f2bd025a7edad
Allaine Tan <allainericci@gmail.com>
2024-02-20 17:35:41 +0800

    feat: add click handler to LargeButton.svelte

--- src/lib/components/LargeButton.svelte
+++ src/lib/components/LargeButton.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import {Icon, ArrowDownTray, Plus, Trash} from "svelte-hero-icons"
-    export let text: string;
     export let icon: string = "";
     // export let is_small: boolean;
 
@@ -9,7 +8,9 @@
 <button 
     type="button" 
     class="hover:bg-[#24292F]/90 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+    on:click
 > 
+
     {#if icon == "download"}
         <Icon src="{ArrowDownTray}" macro size="24"/>
     {:else if icon == "add"}
@@ -17,7 +18,8 @@
     {:else if icon == "delete"}
         <Icon src="{Trash}" macro size="24"/>        
     {/if}
-    <p>{text}</p>
+
+    <slot />
 </button>
 
 <style>
