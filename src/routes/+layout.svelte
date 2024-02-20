74b3c4a304c65a1951aaad52c76d573544f5be9e
Allaine Tan <allainericci@gmail.com>
2024-02-20 20:27:50 +0800

    style: update global font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,11 @@
 </script>
 
 <slot />
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Public+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+	:global(body){
+		font-family: "Public Sans", sans-serif;
+	}
+</style>
