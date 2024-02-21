5b1b063991bbfe684c41ea35b9e98ae8be7e79a8
Allaine Tan <allainericci@gmail.com>
2024-02-21 22:36:25 +0800

    feat: add Hero.svelte component with graphic

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,16 @@
+<script>
+    import img from '../assets/hero-graphic.svg'
+</script>
+
+<div class="flex img-container items-center">
+    <img src={img} alt="card-svg"/>
+</div>
+
+<style>
+.img-container{
+    background-color: #F6FAF4;
+    border-radius: 20px;
+    height: 100%;
+    width: 100%;
+}
+</style>
