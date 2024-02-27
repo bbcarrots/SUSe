86d2e07c0283b6a32474475a56b69784b79060a5
gabrielcalubayan72 <90253997+gabrielcalubayan72@users.noreply.github.com>
2024-02-28 01:08:34 +0800

    test: vitest case for insertStudent method

--- tests/vitestcases.ts
+++ tests/vitestcases.ts
@@ -1,5 +1,19 @@
 import { expect, test } from 'vitest'
+import { Student } from "$lib/classes/student";
 
 test('adds 1 + 2 to equal 3', () => {
     expect(1 + 2).toBe(3)
   })
+
+describe('Student', () => {
+  let studentInstance: Student;
+
+  beforeEach(() => {
+    studentInstance = new Student(202106299, 'abcdefghijkl', 'gscalubayan', 'password', 'Gabriel Aldrich', 'S', 'Calubayan', 'College of Engineering', 'BS Computer Science', '09123456789');
+  });
+
+  it('should insert student in database', () => {
+    expect(studentInstance.insertStudent()).toBe(true)
+  });
+
+})
