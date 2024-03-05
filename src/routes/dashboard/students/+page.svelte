e34c4f2c568608f911e39cf8b97c82d50c681f0d
allainerain <allainericci@gmail.com>
2024-03-05 21:39:09 +0800

    feat: connect Table.svelte to student dashboard using dummy data (for now)

--- src/routes/dashboard/students/+page.svelte
+++ src/routes/dashboard/students/+page.svelte
@@ -1,1 +1,29 @@
-Table
+<script>
+	import Table from "$lib/components/Table.svelte";
+	
+	// dummy data for now, assume already filtered and processed and in order in the table
+	let headers = ["Name", "Student Number", "Email", "Phone Number", "College", "Program", "Actions"]
+
+	let students = [
+		{
+			name: 'Juan A Dela Cruz',
+			sn: 202012345,
+			email: 'jadelacruz',
+			phoneNumber: '09999999999',
+			college: 'COE',
+			program: 'CS',
+			isEnrolled: 0
+		},
+		{
+			name: 'Juan A Dela Cruz',
+			sn: 202012345,
+			email: 'jadelacruz',
+			phoneNumber: '09999999999',
+			college: 'COE',
+			program: 'CS',
+			isEnrolled: 0
+		}
+	]
+</script>
+
+<Table headers={headers} information = {students}/>
