7fc1807c12393e33d8558957146753f77e5bc084
allainerain <allainericci@gmail.com>
2024-03-07 00:07:47 +0800

    style: Table.svelte font update and hoverable

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -5,31 +5,35 @@
 </script>
 
 <!-- <TableSearch /> -->
-<Table>
-    <TableHead>
-      <TableHeadCell class="!p-4">
-        <Checkbox />
-      </TableHeadCell>
-      {#each headers as header}
-        <TableHeadCell> {header} </TableHeadCell>
-      {/each}
-    </TableHead>
-    <TableBody>
-      {#each information as info}
-        <TableBodyRow>
-          <TableBodyCell class="!p-4">
-            <Checkbox />
-          </TableBodyCell>
-          {#each Object.entries(info) as [field, value]}
-            {#if field!=="isEnrolled"}
-              <TableBodyCell>{value}</TableBodyCell>
-            {/if}
-          {/each}
-          <TableBodyCell>
-            <a href="/tables" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Edit</a>
-            <a href="/tables" class="font-medium text-red-600 hover:underline dark:text-red-500">Remove</a>
-          </TableBodyCell>
-        </TableBodyRow>
-      {/each}
-    </TableBody>
-  </Table>
+<Table hoverable={true}>
+  <TableHead theadClass="sentencecase">
+    <TableHeadCell class="!p-4">
+      <Checkbox />
+    </TableHeadCell>
+    {#each headers as header}
+      <TableHeadCell> <p>{header}</p> </TableHeadCell>
+    {/each}
+  </TableHead>
+  <TableBody>
+    {#each information as info}
+      <TableBodyRow>
+        <TableBodyCell class="!p-4">
+          <Checkbox />
+        </TableBodyCell>
+        {#each Object.entries(info) as [field, value]}
+          {#if field!=="isEnrolled"}
+            <TableBodyCell><p>{value}</p></TableBodyCell>
+          {/if}
+        {/each}
+        <TableBodyCell>
+          <a href="/tables" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Edit</a>
+          <a href="/tables" class="font-medium text-red-600 hover:underline dark:text-red-500">Remove</a>
+        </TableBodyCell>
+      </TableBodyRow>
+    {/each}
+  </TableBody>
+</Table>
+
+<style>
+
+</style>
