54918c865746da1213832cdeb8a9bd0b3d0960c3
gabrielcalubayan72 <90253997+gabrielcalubayan72@users.noreply.github.com>
2024-03-07 22:24:49 +0800

    test: new branch for tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,45 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
+import { Student, State } from '$lib/classes/Student'; 
 
 describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
 		expect(1 + 2).toBe(3);
 	});
 });
+
+
+describe('Student', () => {
+  let studentInstance: Student;
+
+  beforeEach(() => {
+    studentInstance = new Student(198712345, "rfid12345", "Eheads", "Password1234", "Ely", "B", "Buendia", "College of Mass Communications", "BA Film", "09123456789");
+  });
+
+  it('success: inserted student in database', async () => {
+    const expectedState: State = { // returned state upon successful insert into database
+      success: true,
+			value: null,
+			error: null
+    } 
+	
+    await expect(studentInstance.insertStudent()).resolves.toBe(expectedState)
+  });
+
+  it('error: inserting already existing record', async () => {
+    const expectedState: State = { // returned state upon successful insert into database
+      success: false,
+      value: null,
+      error: "Error: Student sn/username already exists in database"
+    } 
+
+    studentInstance.insertStudent(); // insert it first
+
+    //then insert for a second time
+    await expect(studentInstance.insertStudent()).resolves.toBe(expectedState)
+  });
+
+
+  // it('error: inserting when no database connection', async() => {
+
+  // });
+})
