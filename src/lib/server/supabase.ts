2a9fee7a923ec0cf2d68edbf66e69b92bfe26ceb
ZARAH FLORO <znfloro@up.edu.ph>
2024-03-10 19:58:09 +0800

    feat: add updateStudentDB

--- src/lib/server/supabase.ts
+++ src/lib/server/supabase.ts
@@ -67,8 +67,32 @@ export async function insertStudentDB(student: Student): Promise<StudentResponse
 	};
 }
 
-// export async function updateStudentDB(
-// 	student: Student,
-// 	oldSN: number,
-// 	oldUsername: string
-// ): Promise<DBState>
+export async function updateStudentDB(student: Student): Promise<StudentResponse> {
+	/* Updates a student record based using their student number and username.
+    NOTE: Cannot update the student number or username of a student. Need to delete and register again. */
+	const { error } = await supabase
+		.from('student')
+		.update({
+			first_name: student.firstName,
+			middle_initial: student.middleInitial,
+			last_name: student.lastName,
+			college: student.college,
+			program: student.program,
+			phone_number: student.phoneNumber
+		})
+		.match({ student_number: student.studentNumber, username: student.username });
+
+	if (error) {
+		return {
+			success: false,
+			studentRaws: null,
+			error: error.message
+		};
+	}
+
+	return {
+		success: true,
+		studentRaws: null,
+		error: null
+	};
+}
