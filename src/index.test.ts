be06f5b9c330af274652c8303d7a491d886fb2b5
gabrielcalubayan72 <90253997+gabrielcalubayan72@users.noreply.github.com>
2024-03-13 10:18:33 +0800

    test: commented out tests muna

--- src/index.test.ts
+++ src/index.test.ts
@@ -10,106 +10,106 @@ describe('it should add 2 and 3 properly', () => {
 });
 
 
-describe('Student.insertStudent', () => {
-  let studentInstance: Student;
+// describe('Student.insertStudent', () => {
+//   let studentInstance: Student;
 
-  beforeEach(async () => {
-    studentInstance = new Student(198712345, "rfid12345", "ebbuendia", "Password1234", "Ely", "B", "Buendia", "College of Mass Communications", "BA Film", "09123456789", 1);
-    // need to reset database first before each test
-    const adminConn: mysql.Connection | null = await connectAdminMySQL();
-    const deleteQuery = "DELETE FROM students";
-    if(adminConn){
-      adminConn.query(deleteQuery); // delete all entries in database
-    }
+//   beforeEach(async () => {
+//     studentInstance = new Student(198712345, "rfid12345", "ebbuendia", "Password1234", "Ely", "B", "Buendia", "College of Mass Communications", "BA Film", "09123456789", 1);
+//     // need to reset database first before each test
+//     const adminConn: mysql.Connection | null = await connectAdminMySQL();
+//     const deleteQuery = "DELETE FROM students";
+//     if(adminConn){
+//       adminConn.query(deleteQuery); // delete all entries in database
+//     }
 
-  });
+//   });
 
-  it('success: inserted student in database', async () => {
-    const expectedState: DBState = { // returned state upon successful insert into database
-      success: true,
-			studentRaws: [],
-			error: null
-    } 
-    await expect(studentInstance.insertStudent()).resolves.toStrictEqual(expectedState)
-  });
+//   it('success: inserted student in database', async () => {
+//     const expectedState: DBState = { // returned state upon successful insert into database
+//       success: true,
+// 			studentRaws: [],
+// 			error: null
+//     } 
+//     await expect(studentInstance.insertStudent()).resolves.toStrictEqual(expectedState)
+//   });
 
-  it('error: inserting with student number already in use', async () => {
-    const expectedState: StudentState = { // returned state upon unsuccessful insert into database
-      success: false,
-      studentRaws: [],
-      error: "Error: Student sn/username already exists in database"
-    } 
-    let studentSameSN = new Student(198712345, "rfid12346", "ebbuendia2", "Password1234", "Ely", "B", "Buendia Jr.", "College of Mass Communications", "BA Film", "09123456789", 1);
+//   it('error: inserting with student number already in use', async () => {
+//     const expectedState: StudentState = { // returned state upon unsuccessful insert into database
+//       success: false,
+//       studentRaws: [],
+//       error: "Error: Student sn/username already exists in database"
+//     } 
+//     let studentSameSN = new Student(198712345, "rfid12346", "ebbuendia2", "Password1234", "Ely", "B", "Buendia Jr.", "College of Mass Communications", "BA Film", "09123456789", 1);
     
-    await studentInstance.insertStudent(); // insert it first
+//     await studentInstance.insertStudent(); // insert it first
 
-    //then insert for a second time
-    await expect(studentSameSN.insertStudent()).resolves.toStrictEqual(expectedState)
-  });
+//     //then insert for a second time
+//     await expect(studentSameSN.insertStudent()).resolves.toStrictEqual(expectedState)
+//   });
 
-  it('error: inserting with username already in use', async () => {
-    const expectedState: StudentState = { // returned state upon unsuccessful insert into database
-      success: false,
-      studentRaws: [],
-      error: "Error: Student sn/username already exists in database"
-    } 
-    let studentSameSN = new Student(198712346, "rfid12346", "ebbuendia", "Password1234", "Ely", "B", "Buendia Jr.", "College of Mass Communications", "BA Film", "09123456789", 1);
+//   it('error: inserting with username already in use', async () => {
+//     const expectedState: StudentState = { // returned state upon unsuccessful insert into database
+//       success: false,
+//       studentRaws: [],
+//       error: "Error: Student sn/username already exists in database"
+//     } 
+//     let studentSameSN = new Student(198712346, "rfid12346", "ebbuendia", "Password1234", "Ely", "B", "Buendia Jr.", "College of Mass Communications", "BA Film", "09123456789", 1);
     
-    await studentInstance.insertStudent(); // insert it first
+//     await studentInstance.insertStudent(); // insert it first
 
-    //then insert for a second time
-    await expect(studentSameSN.insertStudent()).resolves.toStrictEqual(expectedState)
-  });
+//     //then insert for a second time
+//     await expect(studentSameSN.insertStudent()).resolves.toStrictEqual(expectedState)
+//   });
 
 
-  it('error: inserting when no database connection', async() => {
-    const expectedState: DBState = {
-      success: false,
-      studentRaws: [],
-      error: 'Error: No database connection'
-    };
+//   it('error: inserting when no database connection', async() => {
+//     const expectedState: DBState = {
+//       success: false,
+//       studentRaws: [],
+//       error: 'Error: No database connection'
+//     };
 
-    await mysql.createConnection({ // how to disconnect from database?
-      host: 'testhost',
-      user: 'testuser',
-      password: 'testpass',
-      database: 'testdb'
-    });
+//     await mysql.createConnection({ // how to disconnect from database?
+//       host: 'testhost',
+//       user: 'testuser',
+//       password: 'testpass',
+//       database: 'testdb'
+//     });
 
-    await expect(studentInstance.insertStudent()).resolves.toStrictEqual(expectedState)
+//     await expect(studentInstance.insertStudent()).resolves.toStrictEqual(expectedState)
 
-  });
-})
+//   });
+// })
 
-describe('Student.convertToStudent', () => { // not yet working
-  let studentRawInstance: StudentRaw;
+// describe('Student.convertToStudent', () => { // not yet working
+//   let studentRawInstance: StudentRaw;
 
-  beforeEach(async () => {
-    studentRawInstance = {sn: 198712345, 
-      rfid: "rfid12345", 
-      username: "Eheads", 
-      pass: "Password1234", 
-      firstName: "Ely", 
-      middleInitial: "B", 
-      lastName: "Buendia", 
-      college: "College of Mass Communications", 
-      program: "BA Film", 
-      phoneNumber: "09123456789",
-      isEnrolled: 1
-    };
-    // need to reset database first before each test
-    const adminConn: mysql.Connection | null = await connectAdminMySQL();
-    const deleteQuery = "DELETE FROM students";
-    if(adminConn){
-      adminConn.query(deleteQuery); // delete all entries in database
-    }
+//   beforeEach(async () => {
+//     studentRawInstance = {sn: 198712345, 
+//       rfid: "rfid12345", 
+//       username: "Eheads", 
+//       pass: "Password1234", 
+//       firstName: "Ely", 
+//       middleInitial: "B", 
+//       lastName: "Buendia", 
+//       college: "College of Mass Communications", 
+//       program: "BA Film", 
+//       phoneNumber: "09123456789",
+//       isEnrolled: 1
+//     };
+//     // need to reset database first before each test
+//     const adminConn: mysql.Connection | null = await connectAdminMySQL();
+//     const deleteQuery = "DELETE FROM students";
+//     if(adminConn){
+//       adminConn.query(deleteQuery); // delete all entries in database
+//     }
 
-  });
+//   });
 
-  it("success: converted StudentRaw to Student instance", () => {
-    let student: Student;
-    let expectedStudent = new Student(198712345, "rfid12345", "Eheads", "Password1234", "Ely", "B", "Buendia", "College of Mass Communications", "BA Film", "09123456789");
+//   it("success: converted StudentRaw to Student instance", () => {
+//     let student: Student;
+//     let expectedStudent = new Student(198712345, "rfid12345", "Eheads", "Password1234", "Ely", "B", "Buendia", "College of Mass Communications", "BA Film", "09123456789");
 
-    expect(student.convertToStudent(studentRawInstance)).toBe(expectedStudent)
-  });
-});
+//     expect(student.convertToStudent(studentRawInstance)).toBe(expectedStudent)
+//   });
+// });
