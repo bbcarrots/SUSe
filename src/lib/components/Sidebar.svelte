fb3d5fb2c3d8efa5cb377bca2cec7c9557c5026b
allainerain <allainericci@gmail.com>
2024-03-19 19:51:33 +0800

    feat: add Navbar.svelte (basic style)

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { page } from '$app/stores';
+    import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+    import { Icon, CheckCircle, BookOpen, UserGroup, WrenchScrewdriver, ShieldCheck} from 'svelte-hero-icons';
+    let spanClass = 'flex-1 ms-3 whitespace-nowrap';
+    $: activeUrl = $page.url.pathname;
+</script>
+
+<Sidebar {activeUrl} >
+<SidebarWrapper class="h-screen">
+    <SidebarGroup>
+        <SidebarItem label="Usage Logs">
+            <svelte:fragment slot="icon">
+                <span class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"><Icon src="{BookOpen}" micro size="20"/></span>
+            </svelte:fragment>
+        </SidebarItem>
+        <SidebarItem label="Students" {spanClass}>
+            <svelte:fragment slot="icon">
+                <span class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"><Icon src="{UserGroup}" micro size="20"/></span>
+            </svelte:fragment>
+        </SidebarItem>
+        <SidebarItem label="Services" {spanClass}>
+            <svelte:fragment slot="icon">
+                <span class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"><Icon src="{WrenchScrewdriver}" micro size="20"/></span>
+            </svelte:fragment>
+        </SidebarItem>
+        <SidebarItem label="Admin" href="/docs/components/sidebar" active={activeUrl === '/docs/components/sidebar'}>
+            <svelte:fragment slot="icon">
+                <span class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"><Icon src="{ShieldCheck}" micro size="20"/></span>
+            </svelte:fragment>
+        </SidebarItem>`
+    </SidebarGroup>
+</SidebarWrapper>
+</Sidebar>
