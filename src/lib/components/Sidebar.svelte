68d01679ba3dfc16e6eb83820c80120ee4e30325
allainerain <allainericci@gmail.com>
2024-03-20 22:44:29 +0800

    style(Sidebar.svelte): add icon change from outline to solid when hovered

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,32 +1,65 @@
-<script>
+<script lang="ts">
     import { page } from '$app/stores';
     import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
     import { Icon, CheckCircle, BookOpen, UserGroup, WrenchScrewdriver, ShieldCheck} from 'svelte-hero-icons';
     let spanClass = 'flex-1 ms-3 text-[14px] whitespace-nowrap';
     $: activeUrl = $page.url.pathname;
+
+    // keep track of active
+    let active:string = ""
+
+    function setActive(label:string){
+        active = label;
+        console.log(active)
+    }
+
 </script>
 
 <Sidebar {activeUrl} >
     <SidebarWrapper class="h-screen">
         <SidebarGroup>
-            <SidebarItem label="Usage Logs" {spanClass}>
+            <SidebarItem label="Usage Logs" {spanClass} on:mouseenter={() => setActive("Usage Logs")} on:mouseleave={() => setActive("")}>
                 <svelte:fragment slot="icon">
-                    <span class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"><Icon src="{BookOpen}" micro size="20"/></span>
+                    <span class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                        {#if active=="Usage Logs"}
+                            <Icon src="{BookOpen}" solid size="20"/>
+                        {:else}
+                            <Icon src="{BookOpen}" outline size="20"/>
+                        {/if}
+                    </span>
                 </svelte:fragment>
             </SidebarItem>
-            <SidebarItem label="Students" {spanClass}>
+            <SidebarItem label="Students" {spanClass} on:mouseenter={() => setActive("Students")} on:mouseleave={() => setActive("")}>
                 <svelte:fragment slot="icon">
-                    <span class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"><Icon src="{UserGroup}" micro size="20"/></span>
+                    <span class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                        {#if active=="Students"}
+                            <Icon src="{UserGroup}" solid size="20"/>
+                        {:else}
+                            <Icon src="{UserGroup}" outline size="20"/>
+                        {/if}
+                    </span>
                 </svelte:fragment>
             </SidebarItem>
-            <SidebarItem label="Services" {spanClass}>
+            <SidebarItem label="Services" {spanClass} on:mouseenter={() => setActive("Services")} on:mouseleave={() => setActive("")}>
                 <svelte:fragment slot="icon">
-                    <span class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"><Icon src="{WrenchScrewdriver}" micro size="20"/></span>
+                    <span class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                        {#if active=="Services"}
+                            <Icon src="{WrenchScrewdriver}" solid size="20"/>
+                        {:else}
+                            <Icon src="{WrenchScrewdriver}" outline size="20"/>
+                        {/if} 
+                    </span>
                 </svelte:fragment>
             </SidebarItem>
-            <SidebarItem label="Admin" href="/docs/components/sidebar" active={activeUrl === '/docs/components/sidebar'} {spanClass}>
+            <SidebarItem label="Admin" href="/docs/components/sidebar" active={activeUrl === '/docs/components/sidebar'} {spanClass} on:mouseenter={() => setActive("Admin")} on:mouseleave={() => setActive("")}>
                 <svelte:fragment slot="icon">
-                    <span class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"><Icon src="{ShieldCheck}" micro size="20"/></span>
+                    <span class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                        {#if active=="Admin"}
+                            <Icon src="{ShieldCheck}" solid size="20"/>
+                        {:else}
+                            <Icon src="{ShieldCheck}" outline size="20"/>
+                        {/if} 
+                    </span>
                 </svelte:fragment>
             </SidebarItem>`
         </SidebarGroup>
