c9e132701fa6eeb064129c81a0a0f449fb567b55
allainerain <allainericci@gmail.com>
2024-04-01 01:19:55 +0800

    chore: add ServiceTableStores.ts template

---
+++ src/lib/stores/ServiceTableStores.ts
@@ -0,0 +1,8 @@
+import { writable, type Readable } from 'svelte/store';
+
+export let sortKey = writable<string>('name');
+export let sortDirection = writable<number>(1);
+export let isEditing = writable<boolean>(false);
+export let sortedItems = writable<Array<any>>([]);
+
+export const formDataStore = writable(new FormData());
