3bb1949c2431473b53e920537b050b48220addeb
allainerain <allainericci@gmail.com>
2024-03-31 12:47:08 +0800

    chore: add types.ts to store all types

---
+++ src/lib/utils/types.ts
@@ -0,0 +1,11 @@
+export type StudentProcessed = {
+    firstName: string,
+    middleName: string,
+    lastName: string,
+    studentNumber: number,
+    email: string,
+    phoneNumber: string,
+    college: string,
+    program: string,
+    isEnrolled: boolean
+}
