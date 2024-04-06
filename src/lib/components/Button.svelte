ef8a77cbba15c140e7a0ee06ee2d092774bb5e9b
allainerain <allainericci@gmail.com>
2024-04-06 16:38:44 +0800

    refactor: turn css styles to tailwind

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -14,58 +14,21 @@
 
 <button 
     type={submit ? 'submit' : 'button'}
-    class="hover:bg-[#24292F]/90 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-    class:inverse={inverse}
-    class:small={small}
-    class:disabled={disabled}
+    class="flex gap-2 py-3 px-6 rounded-md hover:bg-[#24292F]/95 font-medium text-sm inline-flex items-center
+        {inverse ? 'bg-transparent text-black border border-black border-solid hover:bg-[#24292F]/40' : 'bg-black text-white'}
+        {small ? 'py-2 px-4' : 'py-3 px-6'}
+        {disabled ? 'opacity-50 pointer-events-none' : ''}"
     on:click
 > 
-
     {#if icon == "download"}
-        <Icon src="{ArrowDownTray}" macro size={getIconSize()}/>
+        <Icon src={ArrowDownTray} macro size={getIconSize()}/>
     {:else if icon == "add"}
-        <Icon src="{Plus}" macro size={getIconSize()}/>
+        <Icon src={Plus} macro size={getIconSize()}/>
     {:else if icon == "delete"}
-        <Icon src="{Trash}" macro size={getIconSize()}/>        
+        <Icon src={Trash} macro size={getIconSize()}/>        
     {/if}
 
     <p>
         <slot />
     </p>
-
 </button>
-
-<style>
-
-    button{
-        display: flex;
-        gap: 8px;
-        padding: 12px 24px;
-        background-color: var(--suse-black);
-        color: var(--suse-white);
-        border-radius: 5px;
-    }
-
-    .inverse{
-        background-color: transparent;
-        color: var(--suse-black);
-        border-color: var(--suse-black);
-        border-width: 1px;
-    }
-
-    .small{
-        padding: 6px 8px;
-        gap: 4px;
-    }
-
-    .disabled{
-        background-color: var(--suse-black);
-        opacity: 50%;
-        pointer-events: none;
-    }
-
-    .disabled:hover{
-        cursor: not-allowed;
-    }
-
-</style>
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -2,15 +2,6 @@
     import img from '../assets/hero-graphic.svg'
 </script>
 
-<div class="flex img-container items-center">
+<div class="flex items-center bg-[#F6FAF4] rounded-md h-full w-full">
     <img src={img} alt="card-svg"/>
 </div>
-
-<style>
-.img-container{
-    background-color: #F6FAF4;
-    border-radius: 20px;
-    height: 100%;
-    width: 100%;
-}
-</style>
