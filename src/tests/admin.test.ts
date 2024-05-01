3499a36987ead857487e244d066fbd9f21205585
gabrielcalubayan72 <90253997+gabrielcalubayan72@users.noreply.github.com>
2024-05-01 22:22:52 +0800

    test: error test in inserting same admin id

--- src/tests/admin.test.ts
+++ src/tests/admin.test.ts
@@ -11,26 +11,64 @@ describe('sanity/integrity test: it should add properly', () => {
   });
 
 
-  describe('insertAdminDB()', () => {
-    const newAdminNumber = 202100001;
-    const newRFID = 700001;
-    const newNickname = 'Spongebob';
-    const adminInstance: AdminDBObj = {
+describe('insertAdminDB()', () => {
+  const newAdminNumber = 202100001;
+  const newRFID = 700001;
+  const newNickname = 'Spongebob';
+  const adminInstance: AdminDBObj = {
+    admin_id: newAdminNumber,
+    rfid: newRFID,
+    pw: 'password',
+    nickname: newNickname,
+    is_active: false
+  };
+
+  it('success: inserted admin in database', async () => {
+    const expectedState: AdminResponse = {
+      success: true,
+      adminRaws: null,
+      error: null
+    };
+    await expect(insertAdminDB(adminInstance)).resolves.toStrictEqual(expectedState);
+    await deleteAdminDB(newAdminNumber); // clean up dummy entry
+  });
+});
+
+describe('error: insert with admin number in use', async () => {
+  const newAdminNumber = 202100001;
+  const newRFID = 700001;
+  const newNickname = 'Spongebob';
+  const adminInstance: AdminDBObj = {
+    admin_id: newAdminNumber,
+    rfid: newRFID,
+    pw: 'password',
+    nickname: newNickname,
+    is_active: false
+  };
+  beforeEach(async () => {
+    await insertAdminDB(adminInstance);
+  });
+
+  afterEach(async () => {
+    await deleteAdminDB(newAdminNumber);
+  });
+
+  it('error: insert admin with existing ID', async () => {
+    const expectedState: AdminResponse = {
+      success: false,
+      adminRaws: null,
+      error: 'duplicate key value violates unique constraint "admin_admin_id_key"'
+    };
+
+    const dupeAdminInstance: AdminDBObj = {
       admin_id: newAdminNumber,
-      rfid: newRFID,
+      rfid: newRFID + 1,
       pw: 'password',
       nickname: newNickname,
       is_active: false
     };
-  
-    it('success: inserted admin in database', async () => {
-      const expectedState: AdminResponse = {
-        success: true,
-        adminRaws: null,
-        error: null
-      };
-      await expect(insertAdminDB(adminInstance)).resolves.toStrictEqual(expectedState);
-      await deleteAdminDB(newAdminNumber); // clean up dummy entry
-    });
+
+    await expect(insertAdminDB(dupeAdminInstance)).resolves.toStrictEqual(expectedState);
   });
+});
   
