86125efd5ec108feff30c31d54108e4a6dda7d72
gabrielcalubayan72 <90253997+gabrielcalubayan72@users.noreply.github.com>
2024-05-01 22:53:51 +0800

    test: selectAdminDB tests, one skipped

--- src/tests/admin.test.ts
+++ src/tests/admin.test.ts
@@ -129,7 +129,6 @@ describe('updateAdminDB()', async () => {
   });
 });
 
-
 describe('deleteAdminDB()', async () => {
   const newAdminNumber = 202100003;
   const newRFID = 700003;
@@ -170,3 +169,104 @@ describe('deleteAdminDB()', async () => {
   });
 });
   
+describe('selectAdminDB()', async () => {
+  const newAdminNumber = 202100004;
+  const newRFID = 700004;
+
+  const adminInstanceOne: AdminDBObj = {
+    admin_id: newAdminNumber,
+    rfid: newRFID,
+    pw: 'password',
+    nickname: 'Spongebob',
+    is_active: true
+  };
+
+  const adminInstanceTwo: AdminDBObj = {
+    admin_id: newAdminNumber + 1,
+    rfid: newRFID + 1,
+    pw: 'password',
+    nickname: 'Squidward',
+    is_active: true
+  };
+
+  const adminInstanceThree: AdminDBObj = {
+    admin_id: newAdminNumber + 2,
+    rfid: newRFID + 2,
+    pw: 'password',
+    nickname: 'Mr. Krabs',
+    is_active: false
+  };
+  const adminList: AdminDBObj[] = [adminInstanceOne, adminInstanceTwo, adminInstanceThree];
+
+  beforeEach(async () => {
+    for(const admin in adminList){
+      await insertAdminDB(admin);
+    }
+  });
+
+  afterEach(async () => {
+    for(const admin in adminList){
+      await deleteAdminDB(admin.admin_id);
+    }
+  });
+
+  it('success: select single admin with adminID', async () => {
+    const oneAdminFilter: AdminFilter = {
+      adminID: newAdminNumber,
+      nickname: "",
+      isActive: null
+		};
+    const selectOutput = await selectAdminDB(oneAdminFilter);
+
+		if (selectOutput.adminRaws !== null) {
+			const selectOutputID = selectOutput.adminRaws[0].nickname; // extract admin nickname from selected admin
+			// compare selected admin id with inserted admin id 
+			expect(selectOutputID).toStrictEqual('Spongebob');
+		}
+  });
+
+  it('success: select single admin with nickname', async () => {
+    const oneAdminFilter: AdminFilter = {
+      adminID: 0,
+      nickname: "Squidward",
+      isActive: null
+		};
+    const selectOutput = await selectAdminDB(oneAdminFilter);
+
+		if (selectOutput.adminRaws !== null) {
+			const selectOutputID = selectOutput.adminRaws[0].admin_id; // extract admin nickname from selected admin
+			// compare selected admin id with inserted admin id 
+			expect(selectOutputID).toStrictEqual(newAdminNumber + 1);
+		}
+  });
+
+  it.skip('success: select range from isActive', async () => {
+    const activeAdminFilter: AdminFilter = {
+      adminID: 0,
+      nickname: "",
+      isActive: true
+		};
+
+    const selectOutput = await selectAdminDB(activeAdminFilter);
+    const expectedAdmins = ['Spongebob', 'Squidward'];
+
+		if (selectOutput.adminRaws !== null) {
+      const selectOutputNames = selectOutput.adminRaws.map(admin => admin.nickname)
+			// compare selected admin id with inserted admin id 
+			expect(selectOutputNames).toStrictEqual(expectedAdmins);
+		}  });
+
+    it('success: select nonexistent admin', async () => {
+      const oneAdminFilter: AdminFilter = {
+        adminID: 0,
+        nickname: "LeBron James",
+        isActive: null
+      };
+      const selectOutput = await selectAdminDB(oneAdminFilter);
+  
+      if (selectOutput.adminRaws !== null) {
+        const selectOutputID = selectOutput.adminRaws;
+        expect(selectOutputID).toStrictEqual([]);
+      }
+    });
+});
