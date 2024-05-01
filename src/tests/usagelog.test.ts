60a03d01650429bafff2e3c644de2688c797ac82
gabrielcalubayan72 <90253997+gabrielcalubayan72@users.noreply.github.com>
2024-05-01 18:26:11 +0800

    test: before and after each in insertUsageLog

--- src/tests/usagelog.test.ts
+++ src/tests/usagelog.test.ts
@@ -22,56 +22,64 @@ describe('insertUsageLogDB()', async () => {
   const newAdminID = 212300001;
   const newServiceID = 40001;
 
-  // student
-  const studentInstance: StudentDBObj = {
-    sn_id: newSN,
-    rfid: 90001,
-    username: "klamar",
-    pw: "mrmorale",
-    first_name: "Kendrick",
-    middle_initial: "",
-    last_name: "Lamar",
-    college: "College of Social Sciences and Philosophy",
-    program: "BS Psychology",
-    phone_number: "09123456789",
-    is_enrolled: true,
-    is_active: true
-  }
-  await insertStudentDB(studentInstance);
+  beforeEach(async () => {
+    // student
+    const studentInstance: StudentDBObj = {
+      sn_id: newSN,
+      rfid: 90001,
+      username: "klamar",
+      pw: "mrmorale",
+      first_name: "Kendrick",
+      middle_initial: "",
+      last_name: "Lamar",
+      college: "College of Social Sciences and Philosophy",
+      program: "BS Psychology",
+      phone_number: "09123456789",
+      is_enrolled: true,
+      is_active: true
+    }
+    await insertStudentDB(studentInstance);
 
-  // service
-  const serviceInstance: ServiceDBObj = {
-    service_id: newServiceID,
-    service_type_id: 5, 
-    service_name: "Gaming PC",
-    service_type: "Laptop",
-    in_use: false
-  }
-  await insertServiceDB(serviceInstance);
+    // service
+    const serviceInstance: ServiceDBObj = {
+      service_id: newServiceID,
+      service_type_id: 5, 
+      service_name: "Gaming PC",
+      service_type: "Laptop",
+      in_use: false
+    }
+    await insertServiceDB(serviceInstance);
 
-  // admin
-  const adminInstance: AdminDBObj = {
-    admin_id: newAdminID,
-    rfid: 80001,
-    pw: "password",
-    nickname: "Tupac",
-    is_active: true
-  }
-  await insertAdminDB(adminInstance);
+    // admin
+    const adminInstance: AdminDBObj = {
+      admin_id: newAdminID,
+      rfid: 80001,
+      pw: "password",
+      nickname: "Tupac",
+      is_active: true
+    }
+    await insertAdminDB(adminInstance);
+  });
 
-  // usagelog
-  const usageLogInstance: UsageLogDBObj = {
-    ul_id: newULID,
-    sn_id: newSN,
-    admin_id: newAdminID,
-    service_id: newServiceID,
-    service_type: "Laptop", 
-    datetime_start: "2027-04-05 11:06:00+00",
-    datetime_end: "2027-04-05 11:06:59+00"
-  };
+  afterEach(async () => {
+    await deleteServiceDB(newServiceID);
+    await deleteStudentDB(newSN);
+    await deleteAdminDB(newAdminID);
+  });
 
-  it('success: inserted usage log in database', async () => {
-    // returned StudentResponse upon successful insert into database
+  it.todo('success: inserted usage log in database', async () => {
+    // usagelog
+    const usageLogInstance: UsageLogDBObj = {
+      ul_id: newULID,
+      sn_id: newSN,
+      admin_id: newAdminID,
+      service_id: newServiceID,
+      service_type: "Laptop", 
+      datetime_start: "2027-04-05 11:06:00+00",
+      datetime_end: "2027-04-05 11:06:59+00"
+    };
+
+    // returned UsageLogResponse upon successful insert into database
     const expectedState: UsageLogResponse = {
       success: true,
       usageLogRaws: null,
@@ -79,63 +87,60 @@ describe('insertUsageLogDB()', async () => {
     }
     await expect(insertUsageLogDB(usageLogInstance)).resolves.toStrictEqual(expectedState);
     await deleteUsageLogDB(newULID); // clean up dummy entry
-    await deleteServiceDB(newServiceID);
-    await deleteStudentDB(newSN);
-    await deleteAdminDB(newAdminID);
   });
 });
 
-describe.todo('fail: insertUsageLog with same ULID', () => {
-  // create dummy studentInstance for insertion
-  const newULID = 9;
-  const newSN = 205100001;
-  const newAdminID = 212300001;
-  const service_id = 100020;
+// describe.todo('fail: insertUsageLog with same ULID', () => {
+//   // create dummy studentInstance for insertion
+//   const newULID = 9;
+//   const newSN = 205100001;
+//   const newAdminID = 212300001;
+//   const service_id = 100020;
 
-  const usageLogInstance: UsageLogDBObj = {
-    ul_id: newULID,
-    sn_id: newSN,
-    admin_id: newAdminID,
-    service_id: service_id,
-    service_type: "Laptop", 
-    datetime_start: "2027-04-05 11:06:00+00",
-    datetime_end: "2027-04-05 11:06:59+00"
-  };
+//   const usageLogInstance: UsageLogDBObj = {
+//     ul_id: newULID,
+//     sn_id: newSN,
+//     admin_id: newAdminID,
+//     service_id: service_id,
+//     service_type: "Laptop", 
+//     datetime_start: "2027-04-05 11:06:00+00",
+//     datetime_end: "2027-04-05 11:06:59+00"
+//   };
 
-  beforeEach(async () => {
-    await insertUsageLogDB(usageLogInstance); // insert studentInstance first
-  });
+//   beforeEach(async () => {
+//     await insertUsageLogDB(usageLogInstance); // insert studentInstance first
+//   });
 
-  afterEach(async () => {
-    await deleteStudentDB(newStudentNumber); // clean up studentInstance
-  });
+//   afterEach(async () => {
+//     await deleteStudentDB(newStudentNumber); // clean up studentInstance
+//   });
 
-  it('error: inserting with usage log ID already in use', async () => {
-    // returned StudentResponse upon failed insert with existing sn_id
-    const expectedState: StudentResponse = {
-      success: false,
-      studentRaws: null,
-      error: 'duplicate key value violates unique constraint "student_sn_id_key"' // error message from supabase with existing SN
-    }
+//   it('error: inserting with usage log ID already in use', async () => {
+//     // returned StudentResponse upon failed insert with existing sn_id
+//     const expectedState: StudentResponse = {
+//       success: false,
+//       studentRaws: null,
+//       error: 'duplicate key value violates unique constraint "student_sn_id_key"' // error message from supabase with existing SN
+//     }
 
-    // create 2nd dummy studentSameSN with same SN
+//     // create 2nd dummy studentSameSN with same SN
 
-    const studentSameSN: StudentDBObj = {sn_id: newStudentNumber, 
-      rfid: 1002, 
-      username: "dummy", 
-      pw: "1234Password", 
-      first_name: "DummyJr", 
-      middle_initial: "D", 
-      last_name: "Dumdum", 
-      college: "College of Not Dumm", 
-      program: "BS Not Dummy", 
-      phone_number: "09123456789", 
-      is_enrolled: false
-    };
+//     const studentSameSN: StudentDBObj = {sn_id: newStudentNumber, 
+//       rfid: 1002, 
+//       username: "dummy", 
+//       pw: "1234Password", 
+//       first_name: "DummyJr", 
+//       middle_initial: "D", 
+//       last_name: "Dumdum", 
+//       college: "College of Not Dumm", 
+//       program: "BS Not Dummy", 
+//       phone_number: "09123456789", 
+//       is_enrolled: false
+//     };
 
-    // insert studentSameSN, should error
-    await expect(insertStudentDB(studentSameSN)).resolves.toStrictEqual(expectedState)
-  });
+//     // insert studentSameSN, should error
+//     await expect(insertStudentDB(studentSameSN)).resolves.toStrictEqual(expectedState)
+//   });
 
-});
+// });
 
