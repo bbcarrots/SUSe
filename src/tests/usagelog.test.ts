0cbb2d925244c54579f229cd6cfe85ab47cb3d2c
gabrielcalubayan72 <90253997+gabrielcalubayan72@users.noreply.github.com>
2024-05-01 20:47:54 +0800

    test: deleteUsageLogDB tests

--- src/tests/usagelog.test.ts
+++ src/tests/usagelog.test.ts
@@ -95,7 +95,7 @@ describe('insertUsageLogDB()', async () => {
     // insert first UL
     await insertUsageLogDB(usageLogInstance);
 
-    // returned UsageLogResponse upon failed insert with existing sn_id
+    // returned UsageLogResponse upon failed insert with existing ULID
     const expectedState: UsageLogResponse = {
       success: false,
       usageLogRaws: null,
@@ -113,7 +113,7 @@ describe('insertUsageLogDB()', async () => {
       datetime_end: '2027-04-06 11:06:59+00'
     };
 
-    // insert studentSameSN, should error
+    // insert sameULID, should error
     await expect(insertUsageLogDB(sameULID)).resolves.toStrictEqual(expectedState)
 
     await deleteUsageLogDB(newULID);
@@ -207,7 +207,7 @@ describe.todo('updateUsageLogDB()', async () => {
     // insert first UL
     await insertUsageLogDB(usageLogInstance);
 
-    // returned UsageLogResponse upon failed insert with existing sn_id
+    // returned UsageLogResponse upon failed insert with existing ulid
     const expectedState: UsageLogResponse = {
       success: false,
       usageLogRaws: null,
@@ -225,7 +225,95 @@ describe.todo('updateUsageLogDB()', async () => {
       datetime_end: '2027-04-06 11:06:59+00'
     };
 
-    // insert studentSameSN, should error
+    // insert sameULID, should error
     await expect(updateUsageLogDB(sameULID)).resolves.toStrictEqual(expectedState)
+  });
+});
+
+describe('deleteUsageLogDB()', async () => {
+	const newULID = 1020306;
+	const newSN = 205100001;
+	const newAdminID = 212300001;
+	const newServiceID = 40001;
+
+  const studentInstance: StudentDBObj = {
+    sn_id: newSN,
+    rfid: 90001,
+    username: 'klamar',
+    pw: 'mrmorale',
+    first_name: 'Kendrick',
+    middle_initial: '',
+    last_name: 'Lamar',
+    college: 'College of Social Sciences and Philosophy',
+    program: 'BS Psychology',
+    phone_number: '09123456789',
+    is_enrolled: true,
+    is_active: true
+  };
+
+  const serviceInstance: ServiceDBObj = {
+    service_id: newServiceID,
+    service_type_id: 5,
+    service_name: 'Gaming PC',
+    service_type: 'Laptop',
+    in_use: false
+  };
+
+  const adminInstance: AdminDBObj = {
+    admin_id: newAdminID,
+    rfid: 80001,
+    pw: 'password',
+    nickname: 'Tupac',
+    is_active: false
+  };
+
+  const usageLogInstance: UsageLogDBObj = {
+    ul_id: newULID,
+    sn_id: newSN,
+    admin_id: newAdminID,
+    service_id: newServiceID,
+    service_type: 'Laptop',
+    datetime_start: '2027-04-05 11:06:00+00',
+    datetime_end: '2027-04-05 11:06:59+00'
+  };
+
+	beforeEach(async () => {
+		// student
+		await insertStudentDB(studentInstance);
+		await insertServiceDB(serviceInstance);
+		await insertAdminDB(adminInstance);
+    await insertUsageLogDB(usageLogInstance);
+	});
+
+	afterEach(async () => {
+		await deleteServiceDB(newServiceID);
+		await deleteStudentDB(newSN);
+		await deleteAdminDB(newAdminID);
+	});
+
+	it('success: deleted usage log', async () => {
+		// returned UsageLogResponse upon successful insert into database
+		const expectedState: UsageLogResponse = {
+			success: true,
+			usageLogRaws: null,
+			error: null
+		};
+
+		await expect(deleteUsageLogDB(newULID)).resolves.toStrictEqual(expectedState);
+	});
+
+  it('error: deleting with nonexistent ULID', async () => {
+    // insert first UL
+    await insertUsageLogDB(usageLogInstance);
+
+    // returned UsageLogResponse upon failed delete
+    const expectedState: UsageLogResponse = {
+      success: false,
+      usageLogRaws: null,
+      error: "Error: Usage log does not exist" // error message from supabase with existing ULID
+    }
+
+    // insert studentSameSN, should error
+    await expect(deleteUsageLogDB(newULID + 1)).resolves.toStrictEqual(expectedState)
   });
 });
