978d952e29ccc382560670fdc0b9522712f145a8
allainerain <allainericci@gmail.com>
2024-05-01 17:24:06 +0800

    feat: dispatch avail and end service events

--- src/lib/components/ServiceCard.svelte
+++ src/lib/components/ServiceCard.svelte
@@ -1,104 +1,106 @@
-<script lang="ts">    
-    import { onMount, onDestroy } from "svelte";
-    import { formatTime } from "$lib/utils/utils";
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
+	import { formatTime } from '$lib/utils/utils';
 	import { Modal } from 'flowbite-svelte';
-    import Button from "./Button.svelte";
-    import ServiceForm from "./ServiceForm.svelte";
+	import Button from './Button.svelte';
+	import ServiceForm from './ServiceForm.svelte';
 
-    export let serviceName: string;
-    export let available: number;
-    export let src: string;
+	export let serviceType: string;
+	export let available: number;
+	export let src: string;
 
-    let formData:any = {
-        consented: false,
-        data: null
-    };
+	const dispatch = createEventDispatcher(); // for forwarding events
 
-    let timeStarted: string;
-    let timeEnded: string;
-    let timeNow: string;
-    let started = false;
-    let countdown: string = formatTime(0);
+	let formData: any = {
+		consented: false,
+		data: null
+	};
 
-    let popupModalStart = false;
-    let popupModalEnd = false;
+	let timeStarted: string;
+	let timeEnded: string;
+	let timeNow: string;
+	let started = false;
+	let countdown: string = formatTime(0);
 
-    function startService(){
-        started = true;
-        timeStarted = timeNow;
-        countdown = formatTime(0); 
-        popupModalStart = false;
+	let popupModalStart = false;
+	let popupModalEnd = false;
 
-        //todo: add the usage log entry
-    }
+	function startService() {
+		started = true;
+		timeStarted = timeNow;
+		countdown = formatTime(0);
+		popupModalStart = false;
 
-    function endService(){
-        started = false;
-        timeEnded = timeNow;        
-        popupModalEnd = false;
+		dispatch('availService', serviceType);
+	}
 
-        //todo: edit the usage log entry
-    }
+	function endService() {
+		started = false;
+		timeEnded = timeNow;
+		popupModalEnd = false;
 
-    // function to update the current time for the count
-    function updateTimeNow() {
-        const currentTime = new Date();
-        timeNow = currentTime.toLocaleString('en-US', { dateStyle: 'short', timeStyle: 'medium' });
-        countdown = formatTime(currentTime.getTime() - new Date(timeStarted).getTime());
-    }
+		dispatch('endService');
+	}
 
-    let intervalId: ReturnType<typeof setTimeout>;
-    onMount(() => {
-        updateTimeNow(); 
-        intervalId = setInterval(updateTimeNow, 1000);
-    });
+	// function to update the current time for the count
+	function updateTimeNow() {
+		const currentTime = new Date();
+		timeNow = currentTime.toLocaleString('en-US', { dateStyle: 'short', timeStyle: 'medium' });
+		countdown = formatTime(currentTime.getTime() - new Date(timeStarted).getTime());
+	}
 
-    onDestroy(() => {
-        clearInterval(intervalId);
-    });
+	let intervalId: ReturnType<typeof setTimeout>;
+	onMount(() => {
+		updateTimeNow();
+		intervalId = setInterval(updateTimeNow, 1000);
+	});
 
+	onDestroy(() => {
+		clearInterval(intervalId);
+	});
 </script>
 
-<div class="relative max-w-full h-[250px] bg-white rounded-[20px] overflow-hidden p-6 m-4 drop-shadow-[4px_4px_10px_rgba(17,51,17,0.05)]">
-    <h4 class="z-10 top-0 left-0 relative">{serviceName}</h4>
-    {#if started == false}
-        <p>Available: {available}</p>
-    {:else}
-        <p>Time Started: {timeStarted}</p>
-        <p>{countdown}</p>
-    {/if}
-    <div class="z-20 absolute bottom-2 right-2 flex justify-end items-end w-[100px]">
-        {#if started == false}
-            <Button small={true} on:click={() => popupModalStart = true}>Start</Button>
-        {:else}
-            <Button small={true} on:click={() => popupModalEnd = true}>End</Button>
-        {/if}
-    </div>
-    <div class="z-5 absolute w-[70%] -bottom-[30px] -left-[20px] flex justify-end items-end">
-        <img {src} alt="Service icon" class="z-5">
-    </div>
+<div
+	class="relative m-4 h-[250px] max-w-full overflow-hidden rounded-[20px] bg-white p-6 drop-shadow-[4px_4px_10px_rgba(17,51,17,0.05)]"
+>
+	<h4 class="relative left-0 top-0 z-10">{serviceType}</h4>
+	{#if started == false}
+		<p>Available: {available}</p>
+	{:else}
+		<p>Time Started: {timeStarted}</p>
+		<p>{countdown}</p>
+	{/if}
+	<div class="absolute bottom-2 right-2 z-20 flex w-[100px] items-end justify-end">
+		{#if started == false}
+			<Button small={true} on:click={() => (popupModalStart = true)}>Start</Button>
+		{:else}
+			<Button small={true} on:click={() => (popupModalEnd = true)}>End</Button>
+		{/if}
+	</div>
+	<div class="z-5 absolute -bottom-[30px] -left-[20px] flex w-[70%] items-end justify-end">
+		<img {src} alt="Service icon" class="z-5" />
+	</div>
 </div>
-
 
 <!-- Modal for start service confirmation -->
 <Modal bind:open={popupModalStart} size="xs">
-    <ServiceForm {serviceName} bind:formData/>
+	<ServiceForm {serviceType} bind:formData />
 
 	<!-- Action buttons -->
 	<div class="flex justify-center gap-4">
-		<Button on:click={() => popupModalStart = false} inverse={true}>Cancel</Button>
-        <Button disabled={formData.consented == false} on:click={startService}> Start </Button>
+		<Button on:click={() => (popupModalStart = false)} inverse={true}>Cancel</Button>
+		<Button disabled={formData.consented == false} on:click={startService}>Start</Button>
 	</div>
 </Modal>
 
 <!-- Modal for end service confirmation -->
 <Modal bind:open={popupModalEnd} size="xs" autoclose>
-	<div class="text-center">
-	</div>
+	<div class="text-center"></div>
 
 	<!-- Action buttons -->
 	<div class="flex justify-center gap-4">
-		<Button on:click={() => popupModalEnd = false} inverse={true}>Cancel</Button>
-        <Button on:click={endService}> End </Button>
+		<Button on:click={() => (popupModalEnd = false)} inverse={true}>Cancel</Button>
+		<Button on:click={endService}>End</Button>
 	</div>
 </Modal>
--- src/lib/components/ServiceForm.svelte
+++ src/lib/components/ServiceForm.svelte
@@ -1,61 +1,66 @@
 <script lang="ts">
-    import { Tabs, TabItem } from 'flowbite-svelte';
+	import { Tabs, TabItem } from 'flowbite-svelte';
 
-    export let serviceName: string;
-    export let formData = {
-        consented: false,
-        selectedRoom: "None"
-    };
+	export let serviceType: string;
+	export let formData = {
+		consented: false,
+		selectedRoom: 'None'
+	};
 
-    
-    let inactiveClasses = "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
-    let activeClasses = "inline-block p-4 text-suse-green border-b-2 border-suse-green rounded-t-lg active"
+	let inactiveClasses =
+		'inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300';
+	let activeClasses =
+		'inline-block p-4 text-suse-green border-b-2 border-suse-green rounded-t-lg active';
 
-    function handleCheckboxChange(event: any) {
-        const checkbox = event.target;
-        // console.log(checkbox)
+	function handleCheckboxChange(event: any) {
+		const checkbox = event.target;
+		// console.log(checkbox)
 
-        if (checkbox.checked) {
-            formData.consented = true;
-        } else {
-            formData.consented = false;
-        }
-        // console.log(formData)
-    }
-
+		if (checkbox.checked) {
+			formData.consented = true;
+		} else {
+			formData.consented = false;
+		}
+		// console.log(formData)
+	}
 </script>
 
-
-<div class="grid gap-4 text-center items-center">
-    {#if serviceName !== "Discussion Room"}
-        <h5>Liability Agreement</h5>
-        <p>
-            I accept full responsibility for the {serviceName} once it is loaned to me.
-            I agree that I received the {serviceName} in good condition and I understand that I will be charged with corresponding fees or replacement if it is damaged or lost while under my possession. 
-        </p>
-    {:else if serviceName == "Discussion Room"}
-        <Tabs tabStyle='underline' contentClass="" divider={false}>
-            <TabItem open title="New Room" {inactiveClasses} {activeClasses}>
-                <p>
-                    I agree that I will not damage any property in the Discussion Room, refrain from obstructive behaviour and blablabla. 
-                </p>
-            </TabItem>
-            <TabItem title="Join Room" {inactiveClasses} {activeClasses}>
-                <select bind:value={formData.selectedRoom} class="mb-4 border text-[14px] rounded-[5px] px-[16px] py-[12px] border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                    <option value="">Select a room</option>
-                    <option value="option1">Frequency</option>
-                    <option value="option2">Signal</option>
-                    <option value="option3">Something</option>
-                    <option value="option4">Something</option>
-                </select>
-                <p>
-                    I agree that I will not damage any property in the Discussion Room and refrain from obstructive behaviour. 
-                </p>
-            </TabItem>
-        </Tabs>
-    {/if}
-    <form class="flex gap-2 items-center justify-center">
-        <input type="checkbox" id="consent" value="Consented" on:change={handleCheckboxChange}>
-        <label for="checkbox"><p>I agree to these Terms and Conditions.</p></label>
-    </form>
+<div class="grid items-center gap-4 text-center">
+	{#if serviceType !== 'Discussion Room'}
+		<h5>Liability Agreement</h5>
+		<p>
+			I accept full responsibility for the {serviceType} once it is loaned to me. I agree that I received
+			the {serviceType} in good condition and I understand that I will be charged with corresponding
+			fees or replacement if it is damaged or lost while under my possession.
+		</p>
+	{:else if serviceType == 'Discussion Room'}
+		<Tabs tabStyle="underline" contentClass="" divider={false}>
+			<TabItem open title="New Room" {inactiveClasses} {activeClasses}>
+				<p>
+					I agree that I will not damage any property in the Discussion Room, refrain from
+					obstructive behaviour and blablabla.
+				</p>
+			</TabItem>
+			<TabItem title="Join Room" {inactiveClasses} {activeClasses}>
+				<select
+					bind:value={formData.selectedRoom}
+					class="mb-4 block w-full rounded-[5px] border border-gray-300 p-2.5 px-[16px] py-[12px] text-[14px] text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+				>
+					<option value="">Select a room</option>
+					<option value="option1">Frequency</option>
+					<option value="option2">Signal</option>
+					<option value="option3">Something</option>
+					<option value="option4">Something</option>
+				</select>
+				<p>
+					I agree that I will not damage any property in the Discussion Room and refrain from
+					obstructive behaviour.
+				</p>
+			</TabItem>
+		</Tabs>
+	{/if}
+	<form class="flex items-center justify-center gap-2">
+		<input type="checkbox" id="consent" value="Consented" on:change={handleCheckboxChange} />
+		<label for="checkbox"><p>I agree to these Terms and Conditions.</p></label>
+	</form>
 </div>
