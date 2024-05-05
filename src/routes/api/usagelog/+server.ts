f77482f868c9615d31e119a921dbab2773d37716
ZARAH FLORO <znfloro@up.edu.ph>
2024-05-05 19:08:22 +0800

    feat: add POST request

--- src/routes/api/usagelog/+server.ts
+++ src/routes/api/usagelog/+server.ts
@@ -1,6 +1,12 @@
 import { UsageLog } from '$lib/classes/UsageLog.js';
 import { json } from '@sveltejs/kit';
 
+export async function POST({ request }) {
+	/* Handles Update requests for usage log records. */
+	const filter = await request.json();
+	return json(await UsageLog.selectUsageLogs(filter));
+}
+
 export async function PATCH({ request }) {
 	/* Handles Update requests for usage log records. */
 	const updateInfo = UsageLog.toUsageLogDBObj(await request.json());
