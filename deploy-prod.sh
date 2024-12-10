e354c9fb5fc4844b53120420490ec34e890a5074
Zarah Floro <zbnfloro@gmail.com>
2024-12-10 12:23:35 +0800

    fix: vercel deployment

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "prod" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "prod" || "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "engglib1-prod"  ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
   exit 1;
