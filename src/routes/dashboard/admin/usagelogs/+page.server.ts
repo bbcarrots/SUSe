4f9da1d42bd59aab7dc50346373c868dcd7e280b
Zarah Floro <zbnfloro@gmail.com>
2024-12-12 11:30:39 +0800

    fix: remove page.server.ts code

--- src/routes/dashboard/admin/usagelogs/+page.server.ts
+++
@@ -1,6 +0,0 @@
-// import { UsageLog } from '$lib/classes/UsageLog';
-
-// export async function load() {
-// 	/* Loads usage log records from the DB when page is created. */
-// 	return UsageLog.selectUsageLogs();
-// }
